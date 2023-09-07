6e67a8a7498362819115e3189eded0182d620606
viniciushenrique26 <viniciusslavov@gmail.com>
2023-09-06 23:22:02 -0300

    pokemon list update

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,13 +5,13 @@
 // .catch é um método que retorna uma Promise e lida apenas com casos de rejeição. Ele é o mesmo que .then(null, rejection) (ou .then(undefined, rejection)). 
 // .finally é um método que retorna uma Promise. Ele é executado quando a Promise é resolvida ou rejeitada. 
 // O fetch por padrão sempre usa o método GET, mas podemos passar um segundo parâmetro com as configurações da requisição, como o método HTTP, headers, body, mode, cache, redirect, referrer, etc. 
-
+// .path parameters são parâmetros que fazem parte da URL. serve para identificar um recurso específico.
 // porque eu tenho que converter para json? deve converter para json porque o fetch retorna um objeto Response, que representa a resposta a uma solicitação de rede.
 // O método json() retorna um objeto Promise, que contém os dados no formato JSON.  
 // callbacks são funções que são passadas como argumentos para outras funções. Não é recomendado ter callbacks demtro de outros callbacks, pois isso pode gerar o que chamamos de callback hell. 
 // callback hell é quando temos muitos callbacks aninhados, o que torna o código difícil de ler e dar manutenção.  
 // O segundo then sempre irá retornar o primeiro then, ou seja, o segundo then sempre irá receber o retorno do primeiro then.
-
+// os headers de uma requisicao servem para passar informacoes adicionais para o servidor.
 // arrow function é uma forma mais curta de escrever uma função. Trocamos a palavra function por =>. 
 // se a função tiver apenas um parâmetro, podemos omitir os parênteses. 
 
@@ -20,30 +20,60 @@
 
 // O innerHTML é uma propriedade que define ou retorna a sintaxe HTML descrevendo os elementos descendentes. 
 // o .map é um método que retorna um novo array, com base em uma função que recebe um parâmetro. 
+// .join é um método que junta todos os elementos de um array em uma string. 
+// parametros são variáveis que são passadas para uma função.
+// Promisse.All é um método que retorna uma única Promise que resolve quando todas as promises no argumento iterável forem resolvidas ou quando o iterável passado como argumento não contém promises.
+
+const pokemonList = document.getElementById('pokemonList') //essa linha seleciona a classe pokemons.  
+const loadMoreButton = document.getElementById('loadMoreButton') //essa linha seleciona a classe loadMoreButton. 
+const limit = 5; 
+let offset = 0;
+
+const maxRecords = 151; 
 
 
-function convertPokemonToLi(pokemon) {
-    return `
-        <li id="pokemonList" class="pokemon"> 
-            <span class="number">#001</span> 
-            <span class="name">${pokemon.name}</span>
-            <div class="detail">  
-                <ol class="types"> 
-                    <li class="type">cabeca de gelo</li>
-                    <li class="type">quebradeira</li>
-                </ol>
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
-            </div>
-        </li> 
-    `
 
-} 
-const pokemonList = document.getElementById('pokemonList') //essa linha seleciona a classe pokemons. 
-
-pokeApi.getPokemons().then((pokemons = []) => {  
-    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+function loadPokemonItems(offset,limit){ 
    
-    console.log(pokemons)
+    pokeApi.getPokemons(offset,limit).then((pokemons = []) => {  
+        const newHtml = pokemons.map((pokemon) => ` 
+           
+            <li class="pokemon ${pokemon.type}">  
+                
+                <span class="number">#00${pokemon.number}</span> 
+                <span class="name">${pokemon.name}</span> 
 
-}) 
+                <div class="detail">  
+                    <ol class="types"> 
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol> 
+                    <img class="pokemon-image" src="${pokemon.photo}" alt="${pokemon.name}">  
+                    
+                </div> 
+            
+            </li>`).join('') 
+        pokemonList.innerHTML += newHtml; //essa linha adiciona o conteúdo de newHtml dentro de pokemonList.
     
+    }) 
+}  
+
+loadPokemonItems(offset,limit)
+loadMoreButton.addEventListener('click', () => {  
+    offset += limit
+    const qtdRecordNextPage = offset + limit;
+
+    if(qtdRecordNextPage >= maxRecords){  
+        const newLimit = maxRecords - offset;
+        loadPokemonItems(offset,newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    }else {
+        loadPokemonItems(offset,limit)
+
+    }
+})
+
+
+
+    
+
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,13 +1,41 @@
-const pokeApi = {}  
+const pokeApi = {}   
 
-pokeApi.getPokemons = (offset=0, limit=10) => {
-    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}` 
+function convertPokeApiDetailToPokemon(pokeDetail) { 
+    const pokemon = new Pokemon() //new é uma palavra reservada que cria um novo objeto.
+    pokemon.name = pokeDetail.name 
+    pokemon.number = pokeDetail.id  
+
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name) //essa linha retorna uma lista de tipos de pokemons. 
+    const [type] = types
+    pokemon.types = types
+    pokemon.type = type 
+
+    pokemon.photo = pokeDetail.sprites.versions['generation-v']['black-white']['animated']['front_default'] //essa linha retorna a foto do pokemon.
+    pokemon.photoDesactive = pokeDetail.sprites.versions['generation-v']['black-white']['front_default'] //essa linha retorna a foto do pokemon desativada.
+    return pokemon;
+}
+
+pokeApi.getPokemonDetail = (pokemon) => { 
+   return fetch(pokemon.url) 
+    .then((response) => response.json()) //essa linha retorna a lista de detalhes dos pokemons  
+    .then(convertPokeApiDetailToPokemon) //essa linha retorna uma lista de pokemons.
+}
+
+pokeApi.getPokemons = (offset=0, limit=15) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`  
+
+
     return fetch(url) 
             .then((response) => response.json()) //essa linha converte o response em json. 
-            .then((jsonBody) => jsonBody.results) //essa linha retorna o jsonBody.results. 
-            .catch((error) => console.error(error)) //essa linha imprime o erro no console.
-} 
+            .then((jsonBody) => jsonBody.results) //essa linha pega a lista que está dentro do json. 
+            .then((pokemons)=> pokemons.map(pokeApi.getPokemonDetail)) //essa linha retorna uma lista de promises.
+            .then((detailRequest) => Promise.all(detailRequest)) //essa linha retorna uma promise que resolve quando todas as promises no argumento iterável forem resolvidas ou quando o iterável passado como argumento não contém promises.
+            .then((pokemonsDetails) => pokemonsDetails)
+}  
+
 
 // getPokemons é um método que recebe um pokemon como parâmetro. 
 // pokeApi é um objeto que possui um método chamado getPokemons. 
 // pokeApi.getPokemons é um método que recebe um pokemon como parâmetro. 
+
